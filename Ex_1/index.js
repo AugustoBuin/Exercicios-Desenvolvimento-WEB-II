fd25737b96ba8c82bbc899e88c6fdc74f3055694
AUGUSTO HENRIQUE BUIN <augusto.henrique.buin@gmail.com>
2023-03-21 08:35:55 -0300

    1º- Correções e 3º- adições
    A few code corrections in the first exercise and some additions in the third exercise

--- Ex_1/index.js
+++ Ex_1/index.js
@@ -14,15 +14,16 @@ var count = promptInt("Informe sua Idade: ", "Por favor, digite um número.\nTen
 if (!isNaN(count)) {
     var idade = count;
     var faixaEtaria = '';
-    if (idade >+ 0 && idade < 15) { 
-    faixaEtaria = 'Criança'
-}else if(idade >= 15, idade < 30){
-    faixaEtaria = 'Jovem';
-}else if(idade >= 30, idade < 60){
-    faixaEtaria = 'Adulto';
+    if (idade >= 0 && idade < 15) { 
+    faixaEtaria = 'é Criança'
+}else if(idade >= 15 && idade < 30){
+    faixaEtaria = 'é Jovem';
+}else if(idade >= 30 && idade < 60){
+    faixaEtaria = 'é Adulto';
 } else if(idade > 60){
-    faixaEtaria = 'Idoso';
+    faixaEtaria = 'é Idoso';
+} else {
+    faixaEtaria = 'Ainda não nasceu?!!'
 }};
 
-
-console.log(`Pela sua idade, você é: ${faixaEtaria}`);
+console.log(`Pela sua idade, você: ${faixaEtaria}`);
--- ex_3b/index.js
+++ ex_3b/index.js
@@ -7,20 +7,51 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
+// Routes
 app.get('/', (req, res) => {
-    res.render('formulario')
-  });
-  
-app.post('/', (req, res) => {
     var nome = req.body.nome;
     var sobrenome = req.body.sobrenome; 
     var idade = req.body.idade; 
     var pais = req.body.pais; 
 
-    if (nome == null) {nome == 'Augusto'}
-    else if (sobrenome == null) {sobrenome == 'Henrique Buin'}
-    else if (idade == null) {idade == '20'}
-    else if (pais == null) {pais == 'Brasil'}
+    if (nome == undefined) {nome = 'Augusto'}
+    if (sobrenome == undefined) {sobrenome = 'Henrique Buin'}
+    if (idade == undefined) {idade = '20'}
+    if (pais == undefined) {pais = 'Brasil'}
+
+    // console.log(typeof(nome), typeof(sobrenome), typeof(idade), typeof(pais))
+
+    res.render('index', {nome: nome, sobrenome: sobrenome, idade: idade, pais: pais})
+});
+
+app.get('/formulario', (req, res) => {
+    res.render('formulario')
+  });
+
+app.get('/index', (req, res) => {
+    var nome = req.body.nome;
+    var sobrenome = req.body.sobrenome; 
+    var idade = req.body.idade; 
+    var pais = req.body.pais; 
+
+    if (nome == undefined) {nome = 'Augusto'}
+    if (sobrenome == undefined) {sobrenome = 'Henrique Buin'}
+    if (idade == undefined) {idade = '20'}
+    if (pais == undefined) {pais = 'Brasil'}
+    
+    res.render('index', {nome: nome, sobrenome: sobrenome, idade: idade, pais: pais})
+});
+  
+app.post('/index', (req, res) => {
+    var nome = req.body.nome;
+    var sobrenome = req.body.sobrenome; 
+    var idade = req.body.idade; 
+    var pais = req.body.pais; 
+    
+    if (nome == undefined) {nome = 'Augusto'}
+    if (sobrenome == undefined) {sobrenome = 'Henrique Buin'}
+    if (idade == undefined) {idade = '20'}
+    if (pais == undefined) {pais = 'Brasil'}
 
     res.render('index', {nome: nome, sobrenome: sobrenome, idade: idade, pais: pais})
 });
